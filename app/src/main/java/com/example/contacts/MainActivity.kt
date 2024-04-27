fd814452f1f88a3cbfdf3441c8e02dec3b8830f6
NikolayRyabcev <nikolas14@mail.ru>
2024-04-27 18:46:32 +0300

    первые данные

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.contacts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.contacts.ui.theme.ContactsTheme
 
@@ -38,8 +41,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ContactDetails(){
-
+fun ContactDetails(item:Contact){
+    Box{
+        Image(painter=painterResource(id=R.drawable.baseline_circle_24),
+            contentDescription = null)
+    }
 }
 
 @Preview(showBackground = true)
