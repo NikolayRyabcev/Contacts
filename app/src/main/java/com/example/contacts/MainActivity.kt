2132c32f2d570649f11d2ab46c823dea8834fe1a
NikolayRyabcev <nikolas14@mail.ru>
2024-04-29 13:56:06 +0300

    Начата отрисовка

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.contacts.ui.theme.ContactsTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ContactDetails("Android")
                 }
             }
         }
@@ -33,25 +37,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun ContactDetails(item:Contact){
-    Box{
-        Image(painter=painterResource(id=R.drawable.baseline_circle_24),
-            contentDescription = null)
+fun ContactDetails(name: String, modifier: Modifier = Modifier) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            modifier=Modifier.size(30.dp, 30.dp),
+            alignment = Alignment.Center,
+            painter = painterResource(id = R.drawable.baseline_circle_24),
+            contentDescription = null
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ContactsTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+            content = {
+                ContactDetails("Android")
+            }
+        )
     }
 }
